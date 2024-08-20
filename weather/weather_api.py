7dc381d8a58f6338719c8cbf499d0e1fa719a7e8
Viktor Bazatyn <bazatinviktor@gmail.com>
2024-08-20 03:47:24 +0300

    create a get_weather_conditions function

--- weather/weather_api.py
+++ weather/weather_api.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 import requests
 from dotenv import dotenv_values
 from pathlib import Path
@@ -25,9 +27,31 @@ def get_weather_coordinates(city_name):
         return None, None
 
 
-city = input('Enter city name: ')
-lat, lon = get_weather_coordinates(city)
+def get_weather_conditions(latitude, longitude):
+    API_URL = f'http://api.openweathermap.org/data/2.5/weather?lat={latitude}&lon={longitude}&appid={API_KEY}&units=metric'
+    response = requests.get(API_URL)
+
+    if response.status_code == 200:
+        data = response.json()
+        if data:
+            temperature = data['main']['temp']
+            humidity = data['main']['humidity']
+            weather = data['weather'][0]['description']
+            time_getting = datetime.now()
+            return temperature, humidity, weather, time_getting
+        else:
+            print('No weather data available.')
+            return None, None, None, None
+    else:
+        print(f"Error: {response.status_code}, {response.text}")
+        return None, None, None, None
+
+
+city_name = input('Enter city name: ')
+lat, lon = get_weather_coordinates(city_name)
 if lat is not None and lon is not None:
-    print(f"Coordinates for {city}: Latitude = {lat}, Longitude = {lon}")
+    temp, humidity, weather_desc, time_getting = get_weather_conditions(lat, lon)
+    if temp is not None:
+        print(f"Temperature: {temp}°C, Humidity: {humidity}%, Weather: {weather_desc}, Time: {time_getting}")
 else:
-    print("Coordinates not found.")
+    print("Could not retrieve weather data.")

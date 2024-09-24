bbb4cbc8cc99099af862aadfbde19688447a1def
Viktor Bazatyn <bazatinviktor@gmail.com>
2024-09-24 21:34:22 +0300

    Update create_subscription and subscription_detail views to use Celery for API requests

--- weather/views.py
+++ weather/views.py
@@ -1,7 +1,8 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404, redirect
-from weather.models import Subscription
+from weather.models import Subscription, City, Weather
 from weather.forms import SubscriptionForm
+from weather.tasks import get_weather_coordinates, get_weather_conditions
 
 
 def home(request):
@@ -13,9 +14,22 @@ def create_subscription(request):
     if request.method == 'POST':
         form = SubscriptionForm(request.POST)
         if form.is_valid():
+            city_name = form.cleaned_data['city_name']
+
+            result = get_weather_coordinates.delay(city_name)
+            latitude, longitude, country = result.get()
+
+            if not latitude or not longitude:
+                form.add_error('city_name', f"City '{city_name}' does not exist in the weather service.")
+                return render(request, 'create_subscription.html', {'form': form})
+
+            city, created = City.objects.get_or_create(name=city_name, defaults={'country': country})
+
             subscription = form.save(commit=False)
+            subscription.city = city
             subscription.user = request.user
             subscription.save()
+
             return redirect('weather:view_subscriptions')
     else:
         form = SubscriptionForm()
@@ -60,6 +74,33 @@ def delete_subscription(request, id):
 
 
 @login_required
-def subscription_detail(request, id):
-    subscription = get_object_or_404(Subscription, id=id, user=request.user)
-    return render(request, 'subscription_detail.html', {'subscription': subscription})
+def subscription_detail(request, subscription_id):
+    subscription = get_object_or_404(Subscription, id=subscription_id)
+
+    city = subscription.city
+    weather_data = Weather.objects.filter(city=city).order_by('-time_getting').first()
+
+    if not weather_data:
+        coordinates_result = get_weather_coordinates.delay(city.name)
+        coordinates = coordinates_result.get()
+
+        if coordinates and all(coordinates):
+            lat, lon, country = coordinates
+            weather_result = get_weather_conditions.delay(lat, lon)
+            weather_data = weather_result.get()
+
+            if weather_data and all(weather_data):
+                temperature, humidity, weather_description, time_getting = weather_data
+                weather_data = {
+                    'temperature': temperature,
+                    'humidity': humidity,
+                    'weather_description': weather_description,
+                    'time_getting': time_getting,
+                }
+        else:
+            weather_data = None
+
+    return render(request, 'subscription_detail.html', {
+        'subscription': subscription,
+        'weather_data': weather_data
+    })

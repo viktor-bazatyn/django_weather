384703472421a95659a1f3d8e6a1174120287bff
Viktor Bazatyn <bazatinviktor@gmail.com>
2024-09-24 21:28:30 +0300

    replaced city dropdown with manual city input and added validation

--- weather/forms.py
+++ weather/forms.py
@@ -1,12 +1,25 @@
 from django import forms
 from .models import Subscription
+from weather.tasks import get_weather_coordinates
 
 
 class SubscriptionForm(forms.ModelForm):
+    city_name = forms.CharField(max_length=100, label="City",
+                                widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter city'}))
+
     class Meta:
         model = Subscription
-        fields = ['city', 'notification_period']
+        fields = ['city_name', 'notification_period']
         widgets = {
-            'city': forms.Select(attrs={'class': 'form-control'}),
             'notification_period': forms.Select(attrs={'class': 'form-control'}),
         }
+
+    def clean_city_name(self):
+        city_name = self.cleaned_data.get('city_name').strip()
+        result = get_weather_coordinates.delay(city_name)
+        latitude, longitude, country = result.get()
+
+        if not latitude or not longitude:
+            raise forms.ValidationError(f"City '{city_name}' does not exist in the weather service.")
+        return city_name
+
